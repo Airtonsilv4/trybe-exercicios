1b30937ad353bc1b8d94e86283bff766b815174a
Airtonsilv4 <airton.silva19@hotmail.com>
2021-10-27 20:18:39 -0300

    Fix

--- frond-end/bloco-10-introducao-a-react/dia01/exercise-02/src/Footer.jsx
+++ frond-end/bloco-10-introducao-a-react/dia01/exercise-02/src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Footer extends React.Component {
   render() {
     return (
-      <h1>E isso é só o começo... Não esqueça de exportar o componente criado.</h1>
+      <h1>E isso é só o começo...</h1>
     )
   }
 }
